32d94bee6f2d55fad0e4127e12e61035b3442b40
Coelho Branco <pepin33@proton.me>
2025-10-20 18:13:04 -0300

    fix: add base URL for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/yaldabaoth-email-corporate-ctf-project/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
